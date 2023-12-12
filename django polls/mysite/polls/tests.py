1e83a7db5bc2ef0da17e4ae964469af08a68fd77
mariante0dorescu <broinkus@gmail.com>
2023-12-12 16:02:41 +0200

    test tutorial completed

--- django polls/mysite/polls/tests.py
+++ django polls/mysite/polls/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -22,3 +23,58 @@ class QuestionModelTest(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+def createQuestion(question_text, days):
+    #create a question with a number of days offset to now (negative fro q published in the past, positive for the one not yet published)
+
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text = question_text, pub_date = time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        #if no questions, an appropiate message is displayed
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_questions_list"],[])
+
+    def test_past_questions(self):
+        "questions with a pub date in the past are displayed on the index page"
+        question = createQuestion(question_text = "Past questions", days = -30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_questions_list"], [question])
+    
+    def test_future_question(self):
+        #question with a pub date in the future is not displayed on the index page
+        question = createQuestion(question_text = "Past questions", days = 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_questions_list"], [])  
+    
+    def test_future_past_questions(self):
+        "even past and future questions exists only past is displayed"
+        question_past = createQuestion(question_text = "Past questions", days = -30)
+        question_future = createQuestion(question_text = "Past questions", days = 30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_questions_list"], [question_past])
+    
+    def test_two_past_question(self):
+        #question with a pub date in the future is not displayed on the index page
+        question1 = createQuestion(question_text = "Past question 1", days = -30)
+        question2 = createQuestion(question_text = "Past question 2", days = -5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_questions_list"], [question2, question1])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):    
+    #the detail view of a question with a pub_date in the future returns a 404
+        future_question = createQuestion(question_text="Future question", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = createQuestion(question_text="Past question", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
--- django polls/mysite/polls/views.py
+++ django polls/mysite/polls/views.py
@@ -4,8 +4,70 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+
+def detail(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "polls/detail.html", {"question":question})
+
+
+def vote(request, question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  try:
+    selected_choice = question.choice_set.get(pk = request.POST['choice'])
+  except(KeyError, Choice.DoesNotExist):
+    return render(request, "polls/detail.html", {
+      "question":question,
+      "error_message":"You didn't select a choice",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+
+
+def results(request, question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  choices = question.choice_set.all().order_by('-votes')
+  return render(request, "polls/results.html", {"question":question, 'choices': choices})
+  #return HttpResponse('You are looking at the results of question %s' % question_id)
+
+
+class IndexView(generic.ListView):
+  template_name = "polls/index.html"
+  context_object_name = 'latest_questions_list'
+
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+  
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
+
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        question = context['object']  # The current Question object
+
+        # Get the choices for the current question and sort them by votes
+        choices = Choice.objects.filter(question=question).order_by('-votes')
+
+        # Add the question and sorted choices to the context
+        context['question'] = question
+        context['choices'] = choices
+
+        return 
+        
 # def index(request):
 #   latest_questions_list = Question.objects.order_by("-pub_date")[:5]
 #   #output = ", ".join([q.question_text for q in latest_questions_list])
@@ -13,11 +75,6 @@ from django.views import generic
 #   context = {"latest_questions_list" : latest_questions_list}
 #   return HttpResponse(template.render(context, request))
 #   #return HttpResponse('hello')
-
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, "polls/detail.html", {"question":question})
-
   # try:
   #   question = Question.objects.get(pk=question_id)
   # except:
@@ -39,33 +96,6 @@ def detail(request, question_id):
 # def vote(request, question_id):
 #   return HttpResponse('You are voting on question %s' % question_id)
 
-def vote(request, question_id):
-  question = get_object_or_404(Question, pk = question_id)
-  try:
-    selected_choice = question.choice_set.get(pk = request.POST['choice'])
-  except(KeyError, Choice.DoesNotExist):
-    return render(request, "polls/detail.html", {
-      "question":question,
-      "error_message":"You didn't select a choice",
-    })
-  else:
-    selected_choice.votes += 1
-    selected_choice.save()
-    return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
-
-def results(request, question_id):
-  question = get_object_or_404(Question, pk = question_id)
-  choices = question.choice_set.all().order_by('-votes')
-  return render(request, "polls/results.html", {"question":question, 'choices': choices})
-  #return HttpResponse('You are looking at the results of question %s' % question_id)
-
-class IndexView(generic.ListView):
-  template_name = "polls/index.html"
-  context_object_name = 'latest_questions_list'
-
-  def get_queryset(self):
-    return Question.objects.order_by("-pub_date")[:5]
-  
 # class DetailView(generic.DetailView):
 #   model = Question
 #   template_name = "polls/detail.html"
@@ -73,25 +103,3 @@ class IndexView(generic.ListView):
 # class ResultsView(generic.DetailView):
 #   model = Question
 #   template_name = "polls/results.html"
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        question = context['object']  # The current Question object
-
-        # Get the choices for the current question and sort them by votes
-        choices = Choice.objects.filter(question=question).order_by('-votes')
-
-        # Add the question and sorted choices to the context
-        context['question'] = question
-        context['choices'] = choices
-
-        return context
