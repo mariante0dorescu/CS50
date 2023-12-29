d2768404fff857500be90cec0707fce0cf52672f
mariante0dorescu <broinkus@gmail.com>
2023-12-29 16:37:18 +0200

    migrate with empty tables

--- Project2/commerce/auctions/admin.py
+++ Project2/commerce/auctions/admin.py
@@ -4,3 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- Project2/commerce/commerce/settings.py
+++ Project2/commerce/commerce/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'commerce.urls'
 
 TEMPLATES = [
