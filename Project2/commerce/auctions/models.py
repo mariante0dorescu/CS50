027cb7424ea2abf4622d1213ed610dd453f78d3b
mariante0dorescu <broinkus@gmail.com>
2023-12-29 11:21:05 +0200

    created class placeholders

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -6,14 +6,20 @@ from django.utils import timezone
 
 def image_path(instance, filename):
     user_id = instance.user.id
-    #listing_id = instance.id
-
-    #return f'uploads/{user_id}/{listing_id:02d}/{filename}'
     return f'{user_id}/{filename}'
 
 
 class User(AbstractUser):
     pass
 
+
 class Listing(models.Model):
+    pass
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comment(models.Model):
     pass
