22f0aefa1d35097c75796365544cb1e8b3d67133
mariante0dorescu <broinkus@gmail.com>
2023-12-29 21:23:00 +0200

    models2 failed

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -2,20 +2,55 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
-
+#returns an image path based on user id
 def image_path(instance, filename):
     user_id = instance.user.id
     return f'{user_id}/{filename}'
-
 
 class User(AbstractUser):
     pass
 
 
 class Listing(models.Model):
-    pass
 
+    DURATION = (
+        (1, '1 hour'),
+        (3, '3 hours'),
+        (12, '12 hours'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=65, null=False, blank=True)
+    text = models.TextField(null=True)
+    image = models.FileField(upload_to=image_path, blank=True, null=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(limit_value=0.01)], default=0.0)
+    start = models.DateTimeField(default=timezone.now)
+    duration = models.PositiveIntegerField(default=1, choices=DURATION)
+    end = models.DateTimeField()
+
+    @property
+    def calculate_end(self):
+        if self.duration == 1:
+            return self.start + timezone.timedelta(hours=1)
+        elif self.duration == 3:
+            return self.start + timezone.timedelta(hours=3)
+        elif self.duration == 12:
+            return self.start + timezone.timedelta(hours=12)
+        else:
+            return self.start + timezone.timedelta(hours=12)
+
+    def save(self,*args, **kwargs):
+        if not self.end:
+            self.end = self.calculate_end
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['start']
 
 class Bid(models.Model):
     pass
