0432b4dcd38465c44533a24bf9306331b65e11bc
mariante0dorescu <broinkus@gmail.com>
2023-12-29 08:02:32 +0200

    created media files

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def image_path(instance, filename):
+    user_id = instance.user.id
+    #listing_id = instance.id
+
+    #return f'uploads/{user_id}/{listing_id:02d}/{filename}'
+    return f'{user_id}/{filename}'
 
 
 class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
     pass
