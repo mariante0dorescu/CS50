29e8eab7aa8eec1560741d23645f41407adc7e31
mariante0dorescu <broinkus@gmail.com>
2024-01-05 10:40:47 +0200

    moved categories to the layout template

--- Project2_V2/commerce/auctions/views.py
+++ Project2_V2/commerce/auctions/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import *
 
